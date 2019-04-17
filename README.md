b1fd023c87948a69720e11c6640dad541449b74e
eduardo <eduardo27_minotauro>
2019-04-17 10:18:00 -0300

    batch

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # batch
 job-batch
+
+##Teste
+```
+mvn test
+```
+
+##Target
+```
+Você pode encontrar o resultado no arquivo target / test-outputs / greetings.txt :
+
+Hello John Doe!
+Hello Jane Doe!
+```
---
+++ src/main/java/com/batch/model/Person.java
@@ -0,0 +1,17 @@
+package com.batch.model;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class Person {
+
+    private String firstName;
+    private String lastName;
+}
